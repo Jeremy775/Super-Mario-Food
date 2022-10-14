99005b6b327a99d45edc344eadd06f2a9ea2ab8a
Jeremy775 <jlaisne77@gmail.com>
2022-10-14 15:29:39 +0200

    partial menu header

--- admin/partials/menu.php
+++ admin/partials/menu.php
@@ -3,4 +3,18 @@
         <title></title>
         <link rel="stylesheet" href="../../css/admin.css">
     </head>
+
+    <body>
+        <div>
+            <div>
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="manage_admin.php"></a></li>
+                    <li><a href="manage_category.php"></a></li>
+                    <li><a href="manage_food.php"></a></li>
+                    <li><a href="manage_order.php"></a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
 </html>
