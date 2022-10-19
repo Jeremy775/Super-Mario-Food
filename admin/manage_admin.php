e45843e2294a98598b58e42281d31f8018f2b72d
Jeremy775 <jlaisne77@gmail.com>
2022-10-19 13:41:02 +0200

    html manage_admin

---
+++ admin/manage_admin.php
@@ -0,0 +1,22 @@
+<?php include('partials/menu.php'); ?>
+
+<div class="admin_container">
+    <div>
+        <h1>Gérer les Administrateurs</h1>
+
+        <table>
+            <tr>
+                <th>S.N</th>
+                <th>Nom</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
+
